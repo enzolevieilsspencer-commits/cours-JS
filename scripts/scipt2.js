1cbf0c6f290c9bd8d824d3f0dcf37ecb277dfa69
Enzo Levieils-Spencer <enzolevieilsspencer@gmail.com>
2026-02-05 12:21:02 +0100

    correction script2

--- scripts/scipt2.js
+++ scripts/scipt2.js
@@ -1,13 +1,13 @@
-let name = "Manu";
-let équipe1 = "OM";
-let équipe2 = "PSG";
+let nameD = "Manu";
+let team1 = "OM";
+let team2 = "PSG";
 let score1 = Number(prompt("Saisir le score de l'équipe 1"));
 let score2 = Number(prompt("Saisir le score de l'équipe 2"));
 
 if (score1 > score2) {
-  alert(`${équipe1} à battu ${équipe2} Manu est trop heureux`);
+  alert(`${team1} à battu ${team2} ${nameD} est trop heureux`);
 } else if (score1 < score2) {
-  alert(`${équipe1} à battu ${équipe2} Manu a trop la rage`);
+  alert(`${team1} à battu ${team2} ${nameD} a trop la rage`);
 } else {
-  alert(`${équipe1} et ${équipe2} ont fait un match nul Manu a le seum`);
+  alert(`${team1} et ${team2} ont fait un match nul ${nameD} a le seum`);
 }
